07f487a7086af8fe0845204343359c2f4f7e0799
rifqanzalbina <rifqiadiansyahrq@gmail.com>
2025-03-19 05:45:35 +0700

    Update Version to 0.0.3

--- textstat_id/__init__.py
+++ textstat_id/__init__.py
@@ -1,17 +1,13 @@
-
-## Langkah 4: Implementasi Kode Library
-
-### 1. File `__init__.py`
-
 """
-textstat_id - Library untuk analisis statistik teks Bahasa Indonesia
+texstat_id - Library untuk analisis statistik teks Bahasa Indonesia
 """
 
+# Import fungsi-fungsi langsung ke namespace package
 from .analyzer import analisis_dasar, deteksi_kata_tidak_baku, ekstrak_kata_kunci
 from .counter import hitung_frekuensi, hitung_statistik
 from .readability import hitung_keterbacaan
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __all__ = [
     'analisis_dasar',
     'deteksi_kata_tidak_baku',
